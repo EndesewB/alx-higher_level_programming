fdd9246d3a6667ce44b0c2821530471dc7a4c0fc
endesew bantayehu <endesew12@gmail.com>
2023-03-27 09:32:57 -0700

    safe print even indexerror

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    num_pr = 0
-    for i int range(x):
+    total = 0
+    for i in range(x):
         try:
             print(f"{my_list[i]}", end="")
-            num_pr += 1
+            total += 1
         except IndexError:
             break
     print()
-    return (num_pr)
+    return(total)
