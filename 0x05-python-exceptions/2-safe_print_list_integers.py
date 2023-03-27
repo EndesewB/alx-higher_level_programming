bd271bea6a92def314f79f1b00a9bbed76afa33e
endesew bantayehu <endesew12@gmail.com>
2023-03-27 06:43:29 -0700

    safe print of integer

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,4 +9,5 @@ def safe_print_list_integers(my_list=[], x=0):
             break
         except (TypeError, ValueError):
             pass
-        return num_print
+    print()
+    return (num_print)
