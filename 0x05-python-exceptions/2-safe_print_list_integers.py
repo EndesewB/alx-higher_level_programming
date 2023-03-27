e7ec7be21785a1a7347803c1b7c7771c11b3f1f2
endesew bantayehu <endesew12@gmail.com>
2023-03-27 06:48:02 -0700

    safe print of integer

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,8 +5,6 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             num_print += 1
-        except IndexError:
-            break
         except (TypeError, ValueError):
             pass
     print()
