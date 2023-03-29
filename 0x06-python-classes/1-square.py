6105fd6735b334c4c9398f11e31996baa4f16417
endesew bantayehu <endesew12@gmail.com>
2023-03-29 02:13:41 -0700

    a class Square that defines a square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 """Square clas that prints size of the square"""
+
+
 class Square:
     """Defines a square"""
 
-    def __init__(self, size):
-        
+    def __init__(self, size):     
         """initializinng the class
         Args: size - that define the dimension"""
 
