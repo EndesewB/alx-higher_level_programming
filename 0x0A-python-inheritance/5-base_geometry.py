b03ec0806101055ddbd989e590457cc69f37367c
endesew bantayehu <endesew12@gmail.com>
2023-04-10 10:41:05 -0700

    basegeometry class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""module basegeometry"""
+
+
+class BaseGeometry:
+    """This is an empty class basegeometry"""
+    def area(self):
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """
+        Public instance method that validates the value
+        Args:
+            name: A string representing the name of the value.
+            value: The value to be validated.
+        raises: TypeError: If the value is not an integer.
+                ValueError: If the value is less or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f" {name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
