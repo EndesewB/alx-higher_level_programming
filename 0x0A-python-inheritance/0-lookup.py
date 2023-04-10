963f7a7884763f09696b0460a01b409c0111b03b
endesew bantayehu <endesew12@gmail.com>
2023-04-10 09:59:05 -0700

    lookup a function that returns the list of available attributes and methods of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,11 +5,7 @@
 def lookup(obj):
     """the lookup method to display the atribute of onject
     Args:
-        obj: object that contains attributes
-    Returns:
-        attributes
-        """
-
+    obj: object that contains attributes
+    Returns: attributes"""
 
     return dir(obj)
-""" dir is the built-in function that returns the attributes"""
