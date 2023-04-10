578b3222488f4bcb4fa31740d8b1b3882f5b9758
endesew bantayehu <endesew12@gmail.com>
2023-04-10 11:49:12 -0700

    basegeometry inherites

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,36 @@
+#!usr/bin/python3
+"""module basegeometry"""
+
+
+class BaseGeometry:
+    """This is an empty class basegeometry"""
+    def area(self):
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """
+        Public instance method that validates the value
+        Args:
+            name: A string representing the name of the value.
+            value: The value to be validated.
+        raises: TypeError: If the value is not an integer.
+               ValueError: If the value is less or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f" {name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width=0, height=0):
+        """
+        Instantiates a Rectangle object with the given width and height.
+        Args:
+            width: The width of the rectangle. Must be a positive integer.
+            height: The height of the rectangle. Must be a positive integer.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
