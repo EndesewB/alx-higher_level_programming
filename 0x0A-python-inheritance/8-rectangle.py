6b123496012464f5c1a19236bf6509452828d3f7
endesew bantayehu <endesew12@gmail.com>
2023-04-10 11:52:15 -0700

    basegeometry inherites

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -17,9 +17,9 @@ class BaseGeometry:
                ValueError: If the value is less or equal to 0.
         """
         if not isinstance(value, int):
-            raise TypeError(f" {name} must be an integer")
+            raise TypeError(f" {name} must be an integer".format(value))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0".format(value))
 
 
 class Rectangle(BaseGeometry):
