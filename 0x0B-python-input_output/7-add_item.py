c5663fa77248200bb23bd7b0d421059b690e8e8f
endesew bantayehu <endesew12@gmail.com>
2023-04-11 14:37:32 -0700

    adds all arguments to a Python list,

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,9 +3,9 @@
 import sys
 
 if __name__ == "__main__":
-    save_json = __import__('5-save_to_json_file').save_to_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     """importing save_to_json_ file from previous method"""
-    load_json = __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
     """importing load_from_json_file from previous method"""
     try:
         items = load_json("add_item.json")
