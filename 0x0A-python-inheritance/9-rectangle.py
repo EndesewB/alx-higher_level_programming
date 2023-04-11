501b69e2dc5eccd4f1da7bd5d8803499c49ec76d
endesew bantayehu <endesew12@gmail.com>
2023-04-11 00:32:31 -0700

    rectangle inherite basegeometry class

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -19,5 +19,5 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
+        """Prints the rectangle string"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
-    """Prints the rectangle string"""
