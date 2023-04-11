c783b1c85c0dca2ceb1b5cb87bd26c3774e7f0b9
endesew bantayehu <endesew12@gmail.com>
2023-04-11 11:15:50 -0700

    appending text to the existing file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """The module to write and append to the file"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """this method appends the text and prints length of the string/text"""
     with open(filename, 'a', encoding="utf-8") as file:
         file.write(text)
