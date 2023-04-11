7cfaf860dfb2e6d9a429fafe1af6ec2b8ff96533
endesew bantayehu <endesew12@gmail.com>
2023-04-11 14:58:36 -0700

    adds all arguments to a Python list,

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,17 +3,15 @@
 import sys
 
 if __name__ == "__main__":
-    save_to_json_file = \
-            __import__('5-save_to_json_file').save_to_json_file
+    save_json = __import__('5-save_to_json_file').save_to_json_file
     """importing save_to_json_ file from previous method"""
-    load_from_json_file = \
-            __import__('6-load_from_json_file').load_from_json_file
+    load_json = __import__('6-load_from_json_file').load_from_json_file
     """importing load_from_json_file from previous method"""
     try:
-        items = load_from_json_file("add_item.json")
+        items = load_json("add_item.json")
     except FileNotFoundError:
         items = []
-        """initializing the list to empty"""
+    """initializing the list to empty"""
     items.extend(sys.argv[1:])
     """appending files to the existing filename"""
-    save_to_json_file(items, "add_item.json")
+    save_json(items, "add_item.json")
