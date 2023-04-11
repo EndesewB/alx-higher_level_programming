6052f5075fc8303cf6ea60b3363d1b9d6b39224e
endesew bantayehu <endesew12@gmail.com>
2023-04-10 23:20:13 -0700

    basegeometry validation

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -17,6 +17,6 @@ class BaseGeometry:
                 ValueError: If the value is less or equal to 0.
         """
         if type(value) != int:
-            raise TypeError(f" {name} must be an integer".format(name))
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0".format(name))
+            raise ValueError(f"{name} must be greater than 0")
