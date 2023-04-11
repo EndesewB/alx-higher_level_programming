c8da4f588b41c6d1a375c89d601c10bc5225a42b
endesew bantayehu <endesew12@gmail.com>
2023-04-11 02:48:49 -0700

    myint inverter

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Myint class that inverts equality"""
+
+
+class MyInt(int):
+    """MyInt class inverst operators == and !="""
+
+    def __eq__(self, value):
+        """Override == opeartor with != behavior"""
+        return not super().__eq__(value)
+
+    def __ne__(self, other):
+        """Override != operator with == behavior"""
+        return super().__eq__(value)
