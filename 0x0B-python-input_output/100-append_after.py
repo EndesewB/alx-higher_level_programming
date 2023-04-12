c1035785bea86d5e48b29158b9ce7e2793c521b3
endesew bantayehu <endesew12@gmail.com>
2023-04-12 10:37:50 -0700

    appending text

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""module which addis a line of text"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """the function which appends after the existing one """
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        """reads and gets the existing string"""
+
+    with open(filename, 'w') as file:
+        """writes the new string"""
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string + '\n')
