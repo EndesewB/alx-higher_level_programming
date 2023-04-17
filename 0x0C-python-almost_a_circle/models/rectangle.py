28957a4a3329001ae44092e1374b6bf0073b6fd4
endesew bantayehu <endesew12@gmail.com>
2023-04-17 10:33:28 -0700

    rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,4 +72,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """string representation of the rectangle dict"""
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - " \
+                f"{self.width}/{self.height}"
