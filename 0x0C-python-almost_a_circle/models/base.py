477de75e16ccb4fd7945fcb32772db3816e5543e
endesew bantayehu <endesew12@gmail.com>
2023-04-17 13:33:39 -0700

    base class updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,3 +23,13 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save json file to file objects"""
+        if list_objs is None:
+            list_objs = []
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as file:
+            js_str = cls.to_js_str([ob.to_dictionary() for ob in list_objs])
+            file.write(json_str)
