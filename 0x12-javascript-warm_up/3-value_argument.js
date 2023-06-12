b5b187db2a22f2273831b015892e29cda398a7d6
endesew bantayehu <endesew12@gmail.com>
2023-06-12 06:58:49 -0700

    arguments

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,16 +1,21 @@
 #!/usr/bin/node
 
 const args = process.argv.slice(2);
+let counter = 0;
 
-if (args.length === 0) {
+while (args[counter] !== undefined) {
+  counter++;
+}
+
+if (counter === 0) {
   console.log('No argument');
 } else {
-  let countArgs = '';
-  for (let i = 0; i < args.length; i++) {
-    countArgs += args[i];
-    if (i !== args.length - 1) {
-      countArgs += ' ';
+  let concatenateArgs = '';
+  for (let i = 0; i < counter; i++) {
+    concatenateArgs += args[i];
+    if (i !== counter - 1) {
+      concatenateArgs += ' ';
     }
   }
-  console.log(countArgs);
+  console.log(concatenateArgs);
 }
