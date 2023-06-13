efbd624edcc5bf7cf3a526d1925c10d0586b9de9
endesew bantayehu yenesew <endesew12@gmail.com>
2023-06-13 21:36:13 +0000

    factor index

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -6,11 +6,11 @@ const usersByOccurrence = {};
 
 for (const userId in dict) {
   const occurrence = dict[userId];
-  
+
   if (!usersByOccurrence[occurrence]) {
     usersByOccurrence[occurrence] = [];
   }
-  
+
   usersByOccurrence[occurrence].push(userId);
 }
 
