fd15658ab7d1ea2a72ebe2cec50f5242b5101235
endesew bantayehu <endesew12@gmail.com>
2023-07-02 06:54:37 -0700

    python network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-
-# Check if a URL is provided
-if [ $# -eq 0 ]; then
-  echo "Error: No URL provided."
-  exit 1
-fi
-
-url=$1
-
-# Send request and store the response body in a variable
-response=$(curl -s -o /dev/null -w "%{size_download}" "$url")
-
 # Display the size of the response body
-echo "Response body size: $response bytes"
+curl -sI "$1" | awk '/Content-Length/{print $2}'
